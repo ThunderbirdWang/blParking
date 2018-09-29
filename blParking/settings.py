0d1a2d13e5896291b47ca9527023a094abdd6209
AlanWang <wyj@tbird.cn>
2018-09-29 23:45:35 +0800

    create app inputData

--- blParking/settings.py
+++ blParking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inputData',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'blParking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blParkingDb',
+        'USER':'root',
+        'PASSWORD':'1q2w3eMysql',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
