b3173663548b948ead82d8af79b735c45e2df235
AlanWang <wyj@tbird.cn>
2018-10-08 00:22:16 +0800

    添加登录

--- blParking/urls.py
+++ blParking/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inputData import views as inputDataViews
+from login import views as loginViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('home/',inputDataViews.home,name='home'),
     path('commitDatas/',inputDataViews.cmdt),
     path('cmtOk/',inputDataViews.cmtOk),
+    path('login/',loginViews.login),
 ]
--- inputData/views.py
+++ inputData/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from inputData.models import ownerInfo
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required()
 def index(request):
     return HttpResponse('欢迎光临')
 
+@login_required()
 def home(request):
     testList=map(str,range(100))
     return render(request,'home.html',{'string':testList})
 
+@login_required()
 def cmdt(request):
     return render(request,'commitData.html')
 
+@login_required()
 def cmtOk(request):
     roomNo=request.GET['roomNo']
     phoneNo=request.GET['phoneNo']
