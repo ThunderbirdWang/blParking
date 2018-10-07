b51f2970165c2c63cc3bd1ee346891c7aeb9bf06
AlanWang <wyj@tbird.cn>
2018-10-08 03:37:03 +0800

    添加车牌表、注册admin

--- inputData/models.py
+++ inputData/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from login.models import ownerUser
 
 # Create your models here.
 class ownerInfo(models.Model):
-    roomNo=models.CharField(max_length=20)
-    phoneNo=models.CharField(max_length=20)
+    roomNo=models.CharField(max_length=20,unique=True)
+    phoneNo=models.CharField(max_length=20,unique=True)
+
+class carInfo(models.Model):
+    carId=models.ForeignKey(ownerUser,on_delete=models.CASCADE)
+    numberPlate=models.CharField(max_length=10,unique=True)
--- login/models.py
+++ login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ownerUser(models.Model):
+    phoneNo=models.CharField(max_length=20,unique=True)
+    pwd=models.CharField(max_length=256)
+
